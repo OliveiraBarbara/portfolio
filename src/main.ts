f6035f0081a84ed4e2d3bbc5784508010ad66a70
Barbara Oliveira <barbara_oliveira@ufms.br>
2023-03-07 14:51:22 -0300

    Icon

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { enableProdMode } from '@angular/core';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
